408e4c74c2479708687b640bcf08f36e10c868ce
Brad Wilson <brad@lucky-dip.net>
2013-09-14 15:24:36 +1000

    add Winnow::Model.search method

--- lib/winnow/model.rb
+++ lib/winnow/model.rb
@@ -5,6 +5,9 @@ module Winnow
     module ClassMethods
       attr_reader :searchables
 
+      # Sets up any scopes and class methods which can be searched on.
+      # All methods, scopes and fields are disabled by default so folks
+      # can't mess with params and call methods they shouldn't have access to.
       def searchable(*names)
         found = names.select do |name|
           column_names.include?(name.to_s) || respond_to?(name)
@@ -16,6 +19,18 @@ module Winnow
           raise RuntimeError.new("Unknown searchable: #{str}")
         else
           @searchables = names
+        end
+      end
+
+      # Sets up arel queries for the given params.
+      # Anything not defined by a call to #searchable will be ignored.
+      def search(params)
+        params.slice(*searchables).each do |name, value|
+          if column_names.include?(name.to_s)
+            where(name => value)
+          else
+            send(name, value)
+          end
         end
       end
     end
--- spec/winnow/model_spec.rb
+++ spec/winnow/model_spec.rb
@@ -32,4 +32,37 @@ describe Winnow::Model do
       expect { User.searchable(:name, :some_other_method, :another_method) }.to raise_error(RuntimeError, "Unknown searchable: :some_other_method, :another_method")
     end
   end
+
+  describe ".search" do
+    it "should call any class methods defined as searchable" do
+      User.searchable(:name_like)
+      User.should_receive(:name_like).with("Joelle")
+      User.search(name_like: "Joelle")
+    end
+
+    it "should call any scopes defined as searchable" do
+      User.searchable(:email_from)
+      User.should_receive(:email_from).with("gmail.com")
+      User.search(email_from: "gmail.com")
+    end
+
+    it "should set up equality conditions on any fields defined as searchable" do
+      User.searchable(:name)
+      User.should_receive(:where).with(name: "Don Gately")
+      User.search(name: "Don Gately")
+    end
+
+    it "should call all searchables if multiple params passed in" do
+      User.searchable(:name_like, :email)
+      User.should_receive(:where).with(email: "hal@eta.edu")
+      User.should_receive(:name_like).with("Incand")
+      User.search(name_like: "Incand", email: "hal@eta.edu")
+    end
+
+    it "should ignore any non-searchable parameters" do
+      User.searchable(:name)
+      User.should_not_receive(:where)
+      User.search(email: "hal@eta.edu")
+    end
+  end
 end
