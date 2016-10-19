fccf00c88f422e8132b585cc3d86f54d0f4374da
Bharanee Rathna <bharanee@blake.com.au>
2016-10-20 08:33:13 +1100

    migrate specs to rspec 3

--- spec/winnow/model_spec.rb
+++ spec/winnow/model_spec.rb
@@ -6,27 +6,27 @@ describe Winnow::Model do
   describe ".searchable" do
     it "should accept field names" do
       User.searchable(:name)
-      User.searchables.should eq [:name]
+      expect(User.searchables).to eq [:name]
     end
 
     it "should accept field names contains" do
       User.searchable(:name_contains)
-      User.searchables.should eq [:name_contains]
+      expect(User.searchables).to eq [:name_contains]
     end
 
     it "should accept scopes" do
       User.searchable(:name_starts_with)
-      User.searchables.should eq [:name_starts_with]
+      expect(User.searchables).to eq [:name_starts_with]
     end
 
     it "should accept class methods" do
       User.searchable(:email_from)
-      User.searchables.should eq [:email_from]
+      expect(User.searchables).to eq [:email_from]
     end
 
     it "should accept arrays of names" do
       User.searchable(:name, :email_from)
-      User.searchables.should eq [:name, :email_from]
+      expect(User.searchables).to eq [:name, :email_from]
     end
 
     it "should raise an error if an unknown name is passed in" do
@@ -41,32 +41,32 @@ describe Winnow::Model do
   describe ".search" do
     it "should call any class methods defined as searchable" do
       User.searchable(:name_starts_with)
-      User.should_receive(:name_starts_with).with("Joelle").and_call_original
+      expect(User).to receive(:name_starts_with).with("Joelle").and_call_original
       User.search(name_starts_with: "Joelle")
     end
 
     it "should call any scopes defined as searchable" do
       User.searchable(:email_from)
-      User.should_receive(:email_from).with("gmail.com").and_call_original
+      expect(User).to receive(:email_from).with("gmail.com").and_call_original
       User.search(email_from: "gmail.com")
     end
 
     it "should set up equality conditions on any fields defined as searchable" do
       User.searchable(:name)
-      ActiveRecord::Relation.any_instance.should_receive(:where).with(name: "Don Gately")
+      expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with(name: "Don Gately")
       User.search(name: "Don Gately")
     end
 
     it "should set up contains conditions on any fields defined as contains searchable" do
       User.searchable(:name_contains)
-      ActiveRecord::Relation.any_instance.should_receive(:where).with("users.name like ?", "%ate%")
+      expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with("users.name like ?", "%ate%")
       User.search(name_contains: "ate")
     end
 
     it "should call all searchables if multiple params passed in" do
       User.searchable(:email_from, :email)
-      User.should_receive(:email_from).with("Incand").and_return(User) # fake return otherwise our expectations get confused
-      User.should_receive(:where).with(email: "hal@eta.edu").and_call_original
+      expect(User).to receive(:email_from).with("Incand").and_return(User) # fake return otherwise our expectations get confused
+      expect(User).to receive(:where).with(email: "hal@eta.edu").and_call_original
       User.search(email_from: "Incand", email: "hal@eta.edu")
     end
 
@@ -74,34 +74,34 @@ describe Winnow::Model do
       before { User.searchable(:awesome) }
 
       it "should convert 'true' to true" do
-        ActiveRecord::Relation.any_instance.should_receive(:where).with(awesome: true).and_call_original
+        expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with(awesome: true).and_call_original
         User.search(awesome: 'true')
       end
 
       it "should convert 'false' to false" do
-        ActiveRecord::Relation.any_instance.should_receive(:where).with(awesome: false).and_call_original
+        expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with(awesome: false).and_call_original
         User.search(awesome: 'false')
       end
 
       it "should handle true as you'd expect" do
-        ActiveRecord::Relation.any_instance.should_receive(:where).with(awesome: true).and_call_original
+        expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with(awesome: true).and_call_original
         User.search(awesome: true)
       end
 
       it "should handle false as you'd expect" do
-        ActiveRecord::Relation.any_instance.should_receive(:where).with(awesome: false).and_call_original
+        expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with(awesome: false).and_call_original
         User.search(awesome: false)
       end
 
       it "should ignore falsy values (except false)" do
-        User.should_not_receive(:where)
-        ActiveRecord::Relation.any_instance.should_not_receive(:where)
+        expect(User).to_not receive(:where)
+        expect_any_instance_of(ActiveRecord::Relation).to_not receive(:where)
         User.search(awesome: nil)
         User.search(awesome: '')
       end
 
       it "should convert truthy to true" do
-        ActiveRecord::Relation.any_instance.should_receive(:where).with(awesome: true).and_call_original
+        expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with(awesome: true).and_call_original
         User.search(awesome: 'whatever')
       end
     end
@@ -110,23 +110,23 @@ describe Winnow::Model do
       User.searchable(:email)
       scope = User.where(name: "Hal").search(email: "hal@eta.edu").scope
       # TOFIX is there a better way to test this?
-      scope.to_sql.should include %Q{WHERE "users"."name" = 'Hal' AND "users"."email" = 'hal@eta.edu'}
+      expect(scope.to_sql).to include %Q{WHERE "users"."name" = 'Hal' AND "users"."email" = 'hal@eta.edu'}
     end
 
     it "should ignore any non-searchable parameters" do
       User.searchable(:name)
-      User.should_not_receive(:where)
+      expect(User).to_not receive(:where)
       User.search(email: "hal@eta.edu")
     end
 
     it "should ignore any empty arrays" do
-      User.should_not_receive(:where)
+      expect(User).to_not receive(:where)
       User.search(nil)
     end
 
     it "should ignore any blank parameters" do
       User.searchable(:name, :email)
-      ActiveRecord::Relation.any_instance.should_receive(:where).with(email: "mario@eta.edu")
+      expect_any_instance_of(ActiveRecord::Relation).to receive(:where).with(email: "mario@eta.edu")
       User.search(name: "", email: "mario@eta.edu")
     end
 
